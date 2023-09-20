2b51ca379af077e7ef7c20f1d0e2527314494d0f
midas1214 <hsiung.midas@gmail.com>
2023-09-21 03:22:23 +0800

    first commit

---
+++ LetsExercisePython/main.py
@@ -0,0 +1,45 @@
+import cv2
+from cvzone.PoseModule import PoseDetector
+import socket
+
+# Parameters
+width, height = 1280, 1000
+
+# Webcam
+cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+cap.set(3, width)
+cap.set(4, height)
+
+# Pose Detector
+detector = PoseDetector()
+
+posList = []
+# Communication
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
+serverAddressPort = ("127.0.0.1", 5052)
+
+while True:
+    success, img = cap.read()
+    img = detector.findPose(img)
+    img = cv2.flip(img, 1)
+    lmList, bboxInfo = detector.findPosition(img)
+
+    data = []
+    if bboxInfo:
+        # Get the landmark list
+        lmString = ''
+        for landmark in lmList:
+            data.extend([landmark[0], height - landmark[1], landmark[2]])
+            lmString += f'{landmark[1]}, {img.shape[0] - landmark[2]}, {landmark[3]}'
+
+        posList.append(lmString)
+
+        sock.sendto(str.encode(str(data)), serverAddressPort)
+
+    cv2.imshow("Image", img)
+    if cv2.waitKey(1) == ord('q'):
+        break  # press q to quit
+
