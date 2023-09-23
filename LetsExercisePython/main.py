635ef5e6329502e7c7efa04de00f58300bfc938e
salmoniscute <noralin249@gmail.com>
2023-09-23 19:53:30 +0800

    webcam in unity

--- LetsExercisePython/main.py
+++ LetsExercisePython/main.py
@@ -1,6 +1,16 @@
 import cv2
 from cvzone.PoseModule import PoseDetector
 import socket
+import json
+
+#tcp
+tcp_ip = '127.0.0.1'
+tcp_port = 5066
+address = (tcp_ip,tcp_port)
+
+#udp
+udp_ip = '127.0.0.1'
+udp_port = 5052
 
 # Parameters
 width, height = 1280, 1000
@@ -18,8 +28,8 @@ detector = PoseDetector()
 
 #posList = []
 # Communication
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
-serverAddressPort = ("127.0.0.1", 5052)
+udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
+serverAddressPort = (udp_ip, udp_port)
 
 while True:
     success, img = cap.read()
@@ -27,6 +37,17 @@ while True:
     img = cv2.flip(img, 1)
     lmList, bboxInfo = detector.findPosition(img)
 
+    #tcp
+    img_data = {'image':cv2.imencode('.jpg',img)[1].ravel().tolist()}
+    data = json.dumps(img_data)
+
+    #準備連線
+    tcp_sock = socket.socket (socket.AF_INET, socket.SOCK_STREAM) # TCP
+    tcp_sock.connect(address)
+    #傳送資料
+    tcp_sock. sendall(bytes (data,encoding='utf-8'))
+
+    #udp
     data = []
     if bboxInfo:
         # Get the landmark list
@@ -37,9 +58,12 @@ while True:
 
         #posList.append(lmString)
 
-        sock.sendto(str.encode(str(data)), serverAddressPort)
+        udp_sock.sendto(str.encode(str(data)), serverAddressPort)
 
     cv2.imshow("Image", img)
+    cv2.waitKey(10)
+    tcp_sock.close()
+
     if cv2.waitKey(1) == ord('q'):
         break  # press q to quit
 
---
+++ LetsExerciseUnity/Assets/ImageReceive.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+using UnityEngine.UI;
+
+public class ImageReceive : MonoBehaviour
+{
+    Thread receiveThread;
+    TcpClient client;
+    TcpListener listener;
+    int port;
+
+    public RawImage img;
+    byte[] imageDatas = new byte[0];
+    Texture2D tex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitTcp();
+        tex = new Texture2D(1280, 720);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void InitTcp()
+    {
+        port = 5066;
+        print("TCP init");
+        IPEndPoint anyIP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+        listener = new TcpListener(anyIP);
+        listener.Start();
+        receiveThread = new Thread(new ThreadStart(ReceiveData));
+        receiveThread.IsBackground = true;
+        receiveThread.Start();
+    }
+
+    private void OnDestroy()
+    {
+        receiveThread.Abort();
+    }
+
+    private void ReceiveData()
+    {
+        print("received somthing..."); //回應tcp連線
+        try
+        {
+            while (true)
+            {
+                client = listener.AcceptTcpClient();
+                NetworkStream stream = new NetworkStream(client.Client);
+                StreamReader sr = new StreamReader(stream);
+                string jsonData = sr.ReadLine();
+             
+                //讀成圖片
+                Data _imgData = JsonUtility.FromJson<Data>(jsonData);
+                imageDatas = _imgData.image;
+            }
+        }
+        catch (Exception e)
+        {
+            print(e);
+        }
+    }
+
+    public class Data
+    {
+        public byte[] image;
+    }
+
+    private void FixedUpdate()
+    {
+        tex.LoadImage(imageDatas);
+        img.texture = tex;
+    }
+
+}
